76ce9280b2fbcd15e423a4ae114afddce6a4145e
fierce-adventurer <tripathiayush589@gmail.com>
2026-01-03 16:23:15 +0530

    wrote method to find user past similar post based on topic from the database. Limnit by top 5

--- analytics-service/src/main/java/com/fierceadventurer/analyticsservice/repository/UserPostEmbeddingRepository.java
+++ analytics-service/src/main/java/com/fierceadventurer/analyticsservice/repository/UserPostEmbeddingRepository.java
@@ -2,6 +2,7 @@ package com.fierceadventurer.analyticsservice.repository;
 
 import com.fierceadventurer.analyticsservice.entity.UserPostEmbedding;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,12 @@ import java.util.UUID;
 @Repository
 public interface UserPostEmbeddingRepository extends JpaRepository<UserPostEmbedding , UUID> {
     List<UserPostEmbedding> findBySocialAccountId(UUID socialAccountId);
+
+    @Query(value = """
+            SELECT * FROM user_post_embeddings
+            WHERE social_account_id = :socialAccountId
+            ORDER BY embedding <=> :queryVector
+            LIMIT 5
+            """, nativeQuery = true)
+    List<UserPostEmbedding> findMostSimilar(UUID socialAccountId, double[] queryVector);
 }
